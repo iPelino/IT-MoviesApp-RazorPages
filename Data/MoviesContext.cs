497eb1203a41c73bf4e911f44a08b3984a05e7fa
Pelin Mutanguha <pelmut2000@gmail.com>
2019-12-02 16:48:34 +0200

    Sample MoviesApp

---
+++ Data/MoviesContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesApp.Models;
+
+namespace MoviesApp.Data
+{
+    public class MoviesContext: DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options): base(options) {}
+        public DbSet<Models.Movie> Movies { get; set; }
+    }
+}
